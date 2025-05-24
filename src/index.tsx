cb73a50368998703e0fe8061e9d4ed26dec0888e
bb-de <113197046+bb-de@users.noreply.github.com>
2025-05-24 17:47:57 +0200

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,10 @@
+// src/index.tsx
+
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import AccessibilityWidget from './AccessibilityWidget';
 
-const container = document.createElement('div');
-container.id = 'a11y-widget-container';
-document.body.appendChild(container);
-
-ReactDOM.createRoot(container).render(
+ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <AccessibilityWidget />
   </React.StrictMode>
